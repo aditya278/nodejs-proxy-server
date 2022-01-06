a8393cd3f38fedea1f2129b035b05add94d0d0ec
Aditya <aditya.shukla278@gmail.com>
2022-01-06 22:49:00 +0530

    add rate limit and caching

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const cors = require("cors");
+const rateLimit = require('express-rate-limit');
 require("dotenv").config();
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+
+// Rate limiting Middleware
+const limiter = rateLimit({
+    windowMs: 10 * 60 * 1000, // 10 mins
+    max: 5, // A max of 5 requests
+});
+
+app.use(limiter);
+app.set('trust proxy', 1);
 
 // Routes
 app.use('/api', require('./routes'));
--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,18 @@ const express = require("express");
 const router = express.Router();
 const needle = require("needle");
 const url = require("url");
+const apicache = require("apicache");
 
 // Env vars
 const API_BASE_URL = process.env.API_BASE_URL;
 const API_KEY_NAME = process.env.API_KEY_NAME;
 const API_KEY_VALUE = process.env.API_KEY_VALUE;
 
-router.get("/", async (req, res) => {
+// Initialize Cache
+let cache = apicache.middleware;
+
+router.get("/", cache("2 minutes"), async (req, res) => {
   try {
-    
     // Generating the URL Params here
     const params = new URLSearchParams({
       [API_KEY_NAME]: API_KEY_VALUE,
